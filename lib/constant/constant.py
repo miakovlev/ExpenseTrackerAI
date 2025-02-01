aca81e35f98e8a32046cfc1efbcb822db2cc44e5
mikhail <mikhail@>
2025-02-01 18:49:41 +0200

    first commit

---
+++ lib/constant/constant.py
@@ -0,0 +1,60 @@
+CATEGORIES = {
+    "Food & Drinks": [
+        "Restaurants & Cafés",
+       "Coffee",
+        "Groceries & Delivery"
+    ],
+    "Transport": [
+        "Public Transport",
+        "Taxi & Ride-sharing",
+        "Car Expenses"
+    ],
+    "Housing & Utilities": [
+        "Rent/Mortgage",
+        "Utilities & Bills",
+        "Maintenance & Furnishings"
+    ],
+    "Health & Personal Care": [
+        "Healthcare",
+        "Personal Care"
+    ],
+    "Clothing & Accessories": [
+        "Clothing",
+        "Footwear & Accessories"
+    ],
+    "Entertainment": [
+        "Entertainment"
+    ],
+    "Subscriptions & Digital Services": [
+        "Subscriptions"
+    ],
+    "Travel": [
+        "Flights",
+        "Accommodation",
+        "Tours & Rentals"
+    ],
+    "Education": [
+        "Courses",
+        "Books & Materials"
+    ],
+    "Pets": [
+        "Pet Supplies",
+        "Veterinary"
+    ],
+    "Gifts & Charity": [
+        "Gifts",
+        "Donations"
+    ],
+    "Financial Expenses": [
+        "Taxes, Loans & Fees"
+    ],
+    "Investments": [
+        "Investments & Savings"
+    ],
+    "Household Items": [
+        "Cleaning & Household Supplies"
+    ],
+    "Other": [
+        "Other"
+    ]
+}
